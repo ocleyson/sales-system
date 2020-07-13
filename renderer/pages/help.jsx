bb79162efa20a95d1c14b3b3f03f9e2b9a4ef49e
cleysonsk <clbbdev@gmail.com>
2020-07-13 20:22:49 -0300

    add help page

--- renderer/pages/help.jsx
+++ renderer/pages/help.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
-import Link from 'next/link';
+import NavBar from '../components/navbar';
 
 const Help = () => {
   return (
@@ -8,8 +8,28 @@ const Help = () => {
       <Head>
         <title>Ajuda</title>
       </Head>
-      <div>
-        <h1>Help</h1>
+
+      <NavBar title='Ajuda' />
+      
+      <div className="container">
+
+        <h2>CTRL + P: Página de Produtos</h2>
+
+        <h2>CTRL + V: Página de Venda</h2>
+
+        <h2>CTRL + M: Página de Mesas</h2>
+
+        <h2>CTRL + D: Página de Dinheiro</h2>
+
+        <h2>CTRL + A: Página de Ajuda</h2>
+
+        <h2>CTRL + B: Volta para o campo de busca nas páginas de Produtos e Mesas.</h2>
+
+        <h2>CTRL + ALT + L: Limpar todas as movimentações</h2>
+
+        <h2>DELETE: Limpar mesa</h2>
+
+        <h2>ESC: Desmarcar a mesa selecionada</h2>
       </div>
     </React.Fragment>
   );
